4fe1c9c21641aa0f38e66a436735deb1d6421d26
Melissa Queen <melq@cs.washington.edu>
2025-02-05 16:11:56 -0600

    Removing the untested and unused Read-Until feature in this version

--- src/ReadUntil/install_ru.sh
+++
@@ -1,5 +0,0 @@
-python3 -m venv read_until_env
-source read_until_env/bin/activate
-pip install --upgrade pip
-pip install git+https://github.com/nanoporetech/read_until_api
-source read_until_env/bin/activate
