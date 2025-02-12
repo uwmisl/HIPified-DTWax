69668ac8add31391662a4b88ca985c3294264d77
Melissa Queen <melq@cs.washington.edu>
2025-02-11 20:33:33 -0600

    Formatting

--- src/include/DTW.hpp
+++ src/include/DTW.hpp
@@ -31,22 +31,24 @@ All rights reserved. # SPDX-License-Identifier: LicenseRef-NvidiaProprietary
 #endif
 #include <hip/hip_runtime.h>
 #include "datatypes.hpp"
-namespace FullDTW {
+namespace FullDTW
+{
 
 #include "DTW.cpp"
 
-template <typename val_t, typename idx_t>
+  template <typename val_t, typename idx_t>
 
-__host__ void distances(val_t *ref, val_t *query, val_t *dists,
-                        idx_t num_entries, hipStream_t stream,
-                        val_t *device_last_row) {
-// <<<grid_dim, block_dim, shared_mem, stream>>>
-// so, num_entries number of blocks per grid, and WARP_SIZE threads per block
-  DTW<idx_t, val_t><<<num_entries, WARP_SIZE, 0, stream>>>(
-      ref, query, dists, num_entries, device_last_row);
+  __host__ void distances(val_t *ref, val_t *query, val_t *dists,
+                          idx_t num_entries, hipStream_t stream,
+                          val_t *device_last_row)
+  {
+    // <<<grid_dim, block_dim, shared_mem, stream>>>
+    // so, num_entries number of blocks per grid, and WARP_SIZE threads per block
+    DTW<idx_t, val_t><<<num_entries, WARP_SIZE, 0, stream>>>(
+        ref, query, dists, num_entries, device_last_row);
 
-  return;
-}
+    return;
+  }
 
 }; // namespace FullDTW
 
