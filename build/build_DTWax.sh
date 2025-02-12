0777cfa1c187988509e69717b17b5e8bee799329
Melissa Queen <melq@cs.washington.edu>
2025-02-11 20:22:37 -0600

    Updated build script to include more in data_dims.hpp

--- build/build_DTWax.sh
+++ build/build_DTWax.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Ensure a filename argument is provided
 if [ $# -lt 1 ]; then
   echo "Error: Missing argument. Usage: $0 <filename>"
@@ -10,7 +9,7 @@ fi
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 script_name=$(basename "$0")
 file="$1"
-segment_size=${2:-1} # optional, defaults to 1
+segment_size=${2:-1} # optional argument, defaults to 1
 
 # Check if the file exists
 if [ ! -f "$file" ]; then
@@ -37,11 +36,33 @@ If you have a new data file, building using $script_name <your data file> is rec
 #ifndef DATA_DIMS_HPP
 #define DATA_DIMS_HPP
 
+// The number of threads in each warp. This is 64 for AMD and 32 for NVIDIA.
+// This version of DTWax has not been tested on NVIDIA hardware.
+// Because of the shfl_up operations, the calculations will NOT be correct if you have the incorrect number.
+#define WARP_SIZE 64
+
+// Must be no larger than QUERY_LEN. This is the number of query elements in each query batch.
+// This is constrained by the size of shared memory.
+#define QUERY_BATCH_SIZE 64 
+
+// Each thread will process SEGMENT_SIZE elements of the reference sequence at once
+#define SEGMENT_SIZE $segment_size
+
+// Data dimensions
 #define REF_LEN $ref_len
 #define QUERY_LEN $query_len
 #define NUM_READS $num_reads
-#define SEGMENT_SIZE $segment_size
+
+// Derived constants
+#define REF_TILE_SIZE (SEGMENT_SIZE * WARP_SIZE)
 #define REF_BATCH (REF_LEN / REF_TILE_SIZE)
+#define QUERY_BATCH (QUERY_LEN / QUERY_BATCH_SIZE)
+#define NUM_WAVES (QUERY_BATCH_SIZE + (REF_TILE_SIZE - 1) / (SEGMENT_SIZE))
+#define WARP_SIZE_MINUS_ONE (WARP_SIZE - 1)
+#define NUM_WAVES_BY_WARP_SIZE ((NUM_WAVES / WARP_SIZE) * WARP_SIZE)
+#define REF_BATCH_MINUS_ONE (REF_BATCH - 1)
+#define TWICE_WARP_SIZE_MINUS_ONE ((2 * WARP_SIZE) - 1)
+#define RESULT_REG (SEGMENT_SIZE - 1)
 
 #endif // DATA_DIMS_HPP
 EOL
@@ -54,7 +75,7 @@ fi
 
 # Run make
 if make clean; make debug; then
-  echo -e "Successfully built.\nTo run, do ./main_debug $file"
+  echo -e "Successfully built.\nTo run, do ./main $file"
 else
   echo "Error: Build failed."  >&2
   exit 1
